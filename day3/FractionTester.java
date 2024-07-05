1da610bd111c4b8c2d8aed76c304eb9e32534f86
apcsaworkshop <apcsaconsulting@gmail.com>
2024-07-05 13:44:07 -0500

    changed print method to toString

--- day3/FractionTester.java
+++ day3/FractionTester.java
@@ -22,22 +22,22 @@ public class FractionTester
 
 		// Create a Fraction object
 		Fraction f1 = new Fraction(num, den);
-		// Create a Fraction object with a whole number i.e. den=1
+		// Create a Fraction object with a whole number i.e. dem=1
 		Fraction f2 = new Fraction(num);
 		Fraction f3 = new Fraction(7, 2); // f3 = 7/2
 		Fraction f4 = new Fraction(-4); // f4 = -4/1
 		//Fraction f5 = new Fraction();
 
 		// Print fractions example "3/4" 
-		System.out.println( "f1: " + f1.print());
-		System.out.println( "f2: " + f2.print()); 
-		System.out.println( "f3: " + f3.print());
-		System.out.println( "f4: " + f4.print());
+		System.out.println( "f1: " + f1.toString());
+		System.out.println( "f2: " + f2.toString()); 
+		System.out.println( "f3: " + f3.toString());
+		System.out.println( "f4: " + f4.toString());
 
 
 		// Multiply the fractions
 		Fraction product = f1.multiply(f2);
-		System.out.println("f1*f2: " + product.print());
+		System.out.println("f1*f2: " + product.toString());
 
 
 
