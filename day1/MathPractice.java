a1d561e10cb12b13fd087ff872b6ece039590a37
apcsaworkshop <apcsaconsulting@gmail.com>
2024-07-05 13:19:56 -0500

    Updated MathPractice.java

--- day1/MathPractice.java
+++ day1/MathPractice.java
@@ -1,6 +1,6 @@
 /*
- * Math Practice
- * June 24, 2024
+ * Math Practice: based on inputted integers, the sum, quotient*, sqare root and power operations are performed
+ * * July 5, 2024
  * Mrs. Ayodeji 
 */
 import java.util.Scanner;
